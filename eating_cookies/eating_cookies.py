bc2a31a1575270319d4bcc38751539c5f857271e
Erik Sandoval <erik.sandoval@icloud.com>
2019-10-08 14:28:44 -0700

    added notes

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,15 +6,28 @@ import sys
 # a solution that is more efficient than the naive
 # recursive solution
 
+"""
+I have to create a function that takes in a num and show how many different
+ways the cookies can be eaten. with the highest being an increment of three.
+he can eat 1 cookie 1 way, 2 cookies 2 ways, and 3 cookies 4 ways.
+"""
+
+counter = 0
 
 def eating_cookies(n, cache=None):
-    pass
+    # keep count of how many ways there is for him to eat x amount of cookies
+    # possible recursion, set up a basecase
+    global counter
+
+    return counter
 
 
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        num_cookies = int(sys.argv[1])
-        print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(
-            ways=eating_cookies(num_cookies), n=num_cookies))
-    else:
-        print('Usage: eating_cookies.py [num_cookies]')
+print(eating_cookies(2))
+
+# if __name__ == "__main__":
+#     if len(sys.argv) > 1:
+#         num_cookies = int(sys.argv[1])
+#         print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(
+#             ways=eating_cookies(num_cookies), n=num_cookies))
+#     else:
+#         print('Usage: eating_cookies.py [num_cookies]')
