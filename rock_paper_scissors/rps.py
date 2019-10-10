0ee0115cc737453c7c61b4691bac95209c400bb6
Erik Sandoval <erik.sandoval@icloud.com>
2019-10-09 22:55:52 -0700

    Update rps.py

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -5,32 +5,18 @@ import sys
 """
 I need to take in an int and print all the possible permutations.
 So if the number is two I need to loop that. Maybe a nested for loop.
+
+So maybe this problem can be solved similar to how merge sort is solved.
+You keep going to the end of the array until no values and you return the
+solution.
 """
 
 
 def rock_paper_scissors(n):
-    # array to store values
-    # array of rps
-    rps = ['rock', 'paper', 'scissors']
-    perm_arr = []
-
-    def rps_helper():
-        pass
-    # for loop for first element in array.
-    # print(perm_arr)
-    # print(len(perm_arr))
-    # print(len(perm_arr[0]))
-    # for i in range(len(perm_arr)):
-    #     # print(rps_index)
-    #     for j in range(len(perm_arr[0])):
-    #         if (rps_index == 3):
-    #             rps_index = 0
-    #         print(i, j, rps[rps_index])
-    #         perm_arr[i][j] = rps[rps_index]
-    #         rps_index += 1
+    pass
 
 
-rock_paper_scissors(3)
+rock_paper_scissors(2)
 
 # create a predetermined list length.
 rps_arr = [[0, 0], [0, 0], [0, 0]]
